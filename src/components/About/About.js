bbc7529f487d07369c37ee469696304cc7b99b35
Colton Wiethorn <cwiethor@asu.edu>
2020-02-29 22:56:52 -0600

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-// #167 write file documentation on top of all JS files within folders starting with A
+// #167 write file documentation on top of all JS files within folders starting with A 
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
