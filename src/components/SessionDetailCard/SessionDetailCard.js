5594083c465d6dc908d52c2afd5f623f794e91c3
Marcella Sellers <mseller3@asu.edu>
2020-04-02 00:29:05 -0400

    Update to allow date of sessions to be changed

--- src/components/SessionDetailCard/SessionDetailCard.js
+++ src/components/SessionDetailCard/SessionDetailCard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import DateTimePicker from 'react-datetime-picker';
+import APIService from '../APIService/APIService';
+import moment from 'moment';
 
 const styles = {
   container: {
@@ -19,8 +22,53 @@ const styles = {
   },
 };
 
+
 export default withStyles(styles)(
   class SessionDetailCard extends Component {
+    apiService = new APIService();
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        edit: false,
+        session: null,
+        date: null
+      };
+      this.buttonClick = this.buttonClick.bind(this);
+      this.onCalendarChange = this.onCalendarChange.bind(this);
+    };
+
+    buttonClick = () => {
+      if(this.state.edit === true){
+        this.setState({
+          edit: false
+        });
+      } else {
+        this.setState({
+          edit: true
+        });
+      }
+    };
+
+    onCalendarChange = async date => {
+      let row = this.props.row;
+      let newDate = moment(new Date(date)).format('YYYY/MM/DD HH:mm');
+      row.session['date_modified'] = Math.round(date / 1000);
+      row['Date/Time'] = newDate;
+
+      try {
+        await this.apiService.putSession(row.session);
+        this.setState({
+          edit: false,
+          date: newDate
+        });
+        return true;
+      } catch (err) {
+        console.error(err);
+        return false;
+      }
+    };
+
     render() {
       const { classes, title, row } = this.props;
 
@@ -31,10 +79,21 @@ export default withStyles(styles)(
             return typeof entry[1] !== 'object' ? (
               <p className={classes.detailLine} key={index}>
                 <span className={classes.detailTitle}>{`${entry[0]}: `}</span>
-                {entry[1]}
+                {entry[0] !== "Date/Time" && entry[1]}
+                {entry[0] === "Date/Time" && this.state.edit === false &&
+                  (this.state.date !== null ? this.state.date : entry[1])}
+                {entry[0] === "Date/Time" && this.state.edit === true &&
+                  <DateTimePicker
+                      onChange={this.onCalendarChange}
+                      value={new Date(entry[1])}
+                  />}
               </p>
-            ) : null;
+            ) : null
           })}
+          <button onClick={this.buttonClick}>
+            {this.state.edit === true && `Cancel`}
+            {this.state.edit === false && `Edit`}
+          </button>
         </Paper>
       );
     }
